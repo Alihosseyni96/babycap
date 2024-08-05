5279904bbbd34f3463709a589ad2c2eb91c489ca
p.latif <p.latif@DESKTOP-A4CUJRI>
2024-08-05 14:36:24 +0330

    Done!

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'My Site',
-  tagline: 'FINALY I DID IT!!!!!!!!!!!!!!',
+  tagline: 'Deployed On Pages and And Workflow is Done!!!!',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
